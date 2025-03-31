4535cda58231ecddf06bcf1bf2df4184c677d51b
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-03-31 12:40:54 +0900

    modularize nix

---
+++ configs/xdg-configs.nix
@@ -0,0 +1,14 @@
+{ config, ... }: {
+  xdg.configFile = {
+    "fish" = {
+      source = config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/.config/home-manager/configs/fish";
+      recursive = true;
+    };
+    "nvim" = {
+      source = config.lib.file.mkOutOfStoreSymlink
+        "${config.home.homeDirectory}/.config/home-manager/configs/nvim";
+      recursive = true;
+    };
+  };
+}
--- modules/home.nix
+++ modules/home.nix
@@ -1,21 +1,11 @@
 { pkgs, config, ... }: {
+  imports = [ ../configs/xdg-configs.nix ];
+
   programs.home-manager.enable = true;
   home = {
     homeDirectory = "/home/birb";
     username = "birb";
     stateVersion = "24.11";
     packages = with pkgs; [ antora tree firefox vesktop ];
-  };
-  xdg.configFile = {
-    "fish" = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.config/home-manager/configs/fish";
-      recursive = true;
-    };
-    "nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.config/home-manager/configs/nvim";
-      recursive = true;
-    };
   };
 }
