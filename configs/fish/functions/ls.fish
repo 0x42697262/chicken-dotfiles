d811dc36ad06512a96d0dd853e43b8fb1e63d708
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-03-31 11:13:30 +0900

    Try xdg configs

---
+++ configs/fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='lsd -la' --description 'alias ls=lsd -la'
+  lsd -la $argv
+        
+end
--- modules/home.nix
+++ modules/home.nix
@@ -1,9 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   programs.home-manager.enable = true;
   home = {
     homeDirectory = "/home/birb";
     username = "birb";
     stateVersion = "24.11";
     packages = with pkgs; [ antora tree firefox vesktop ];
+  };
+  xdg.configFile = {
+    "fish" = {
+      source = config.lib.file.mkOutOfStoreSymlink "./configs/fish";
+      recursive = true;
+    };
   };
 }
