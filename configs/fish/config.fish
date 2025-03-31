0dda1db03e16c3937a6116de16ae16e836b2683a
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-03-31 11:37:15 +0900

    Change fish greeting to display hostname, cpu, ram, and local ip

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,3 +1,35 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function fish_greeting
+    set_color cyan
+    echo -n (hostname)
+    set_color normal
+
+    echo -n " ("
+    set_color yellow
+    echo -n (ip -4 addr show | awk '/inet / && !/127.0.0.1/ {print $2}' | cut -d/ -f1 | head -n 1)
+    set_color normal
+    echo -n ") | "
+
+    set_color green
+    echo -n " "(top -bn1 | grep "Cpu(s)" | awk '{print $2}')"%"
+    set_color normal
+    echo -n " | "
+
+    set_color magenta
+    echo -n " "(free -m | awk '/Mem/ {print $3 "MB / " $2 "MB"}')
+    set_color normal
+    echo -n " | "
+
+    set_color blue
+    if test -e /sys/class/power_supply/BAT1/capacity
+        echo -n " "(cat /sys/class/power_supply/BAT1/capacity)"%"
+    else
+        echo -n " N/A"
+    end
+
+    set_color normal
+    echo
+end
