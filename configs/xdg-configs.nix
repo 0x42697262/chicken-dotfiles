b993ff7d6cc300bb497b6b7d2767786d89cfa924
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-03-31 12:52:58 +0900

    [configs/kitty] add kitty dotfile and used list for dotfiles

--- configs/xdg-configs.nix
+++ configs/xdg-configs.nix
@@ -1,19 +1,9 @@
-{ config, ... }: {
-  xdg.configFile = {
-    "fish" = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.config/home-manager/configs/fish";
-      recursive = true;
-    };
-    "nvim" = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.config/home-manager/configs/nvim";
-      recursive = true;
-    };
-    "btop" = {
-      source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.config/home-manager/configs/btop";
-      recursive = true;
-    };
+{ config, lib, ... }:
+let
+  configNames = [ "fish" "nvim" "btop" "kitty" ];
+  mkConfig = name: {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.config/home-manager/configs/${name}";
+    recursive = true;
   };
-}
+in { xdg.configFile = lib.genAttrs configNames mkConfig; }
