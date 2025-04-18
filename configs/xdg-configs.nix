f02d487bf0d61a4f1de9cd91386c904cdf44b1a4
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2025-04-18 16:34:56 +0900

    move all configs to .config

--- configs/xdg-configs.nix
+++ configs/xdg-configs.nix
@@ -3,7 +3,12 @@ let
   configNames = [ "fish" "nvim" "btop" "kitty" "git" "mako" "gopass" ];
   mkConfig = name: {
     source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/.config/home-manager/configs/${name}";
+      "${config.home.homeDirectory}/.config/home-manager/configs/.config/${name}";
     recursive = true;
   };
-in { xdg.configFile = lib.genAttrs configNames mkConfig; }
+in {
+xdg.configFile = lib.genAttrs configNames mkConfig;
+home.file = {
+".gnupg" = {source =".gnupg/gpg-agent.conf" };
+};
+}
