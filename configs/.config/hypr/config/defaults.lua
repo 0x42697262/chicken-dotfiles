e9f93c24244e71fd94e8377d4a3f2394bfcbd24e
0x42697262 <19298806+0x42697262@users.noreply.github.com>
2026-07-17 19:50:14 +0900

    use flameshot

--- configs/.config/hypr/config/defaults.lua
+++ configs/.config/hypr/config/defaults.lua
@@ -3,24 +3,26 @@
 -- ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
 return {
-    filemanager = "",
-    applauncher = "pkill rofi || rofi -show combi -modi window,run,combi -combi-modi window,run",
-    terminal    = "kitty",
-    idlehandler = "swayidle -w timeout 300 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000'",
-    capturing   = [[grim -g "$(slurp)" - | swappy -f -]],
-    screenshot  = "grim - | swappy -f -",
+	filemanager = "",
+	applauncher = "pkill rofi || rofi -show combi -modi window,run,combi -combi-modi window,run",
+	terminal = "kitty",
+	idlehandler = "swayidle -w timeout 300 'swaylock -f -c 000000' before-sleep 'swaylock -f -c 000000'",
+	capturing = "flameshot gui",
+	screenshot = "flameshot full",
+	-- capturing   = [[grim -g "$(slurp)" - | swappy -f -]],
+	-- screenshot  = "grim - | swappy -f -",
 
-    -- Monitors
-    internal  = "HDMI-A-1",
-    external  = "HDMI-A-2",
-    viewpoint = "desc:Stargate Technology 0x2700 0x00001A0A",
-    lg        = "desc:LG Electronics LG TV 0x01010101",
-    samsung   = "desc:Samsung Electric Company LS24C33xG H9TX600204",
-    sony4k    = "desc:Sony SONY TV  *00 0x01010101",
+	-- Monitors
+	internal = "HDMI-A-1",
+	external = "HDMI-A-2",
+	viewpoint = "desc:Stargate Technology 0x2700 0x00001A0A",
+	lg = "desc:LG Electronics LG TV 0x01010101",
+	samsung = "desc:Samsung Electric Company LS24C33xG H9TX600204",
+	sony4k = "desc:Sony SONY TV  *00 0x01010101",
 
-    -- Keys
-    left  = "J",
-    right = "Semicolon",
-    up    = "K",
-    down  = "L",
+	-- Keys
+	left = "J",
+	right = "Semicolon",
+	up = "K",
+	down = "L",
 }
